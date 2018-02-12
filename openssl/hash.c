c262c192814ef300fbd809c546bae28de4db952b
warm3snow <warm3snow@gmail.com>
2018-02-12 16:07:14 +0800

    openssl hash test

---
+++ openssl/hash.c
@@ -0,0 +1,35 @@
+#include <openssl/evp.h>
+#include <stdlib.h>
+
+#define SHA512_DIGEST_LENGTH 64 
+
+static void hex_print(const char *name, const unsigned char *buf, size_t len){
+    size_t i;
+    fprintf(stderr, "%s ", name);
+    for (i=0; i<len; i++){
+        fprintf(stderr, "%02X", buf[i]);
+    }
+    fputs("\n", stderr);
+}
+void TEST_EVP_SHA512(){
+    //initialize md ctx
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, EVP_sha512(), NULL);
+    char *data = (char*)"hello";
+    unsigned int len = strlen(data);
+    //compute hash
+    EVP_DigestUpdate(ctx, data, len);
+    unsigned char result[SHA512_DIGEST_LENGTH]={0};
+    EVP_DigestFinal(ctx, result, &len);
+    //print
+    hex_print("sha512", result, SHA512_DIGEST_LENGTH);
+
+
+    //using one func
+    unsigned char result2[SHA512_DIGEST_LENGTH]={0};
+    EVP_Digest("hello", 5, result2, NULL, EVP_sha512(), NULL);
+    hex_print("sha512", result2, SHA512_DIGEST_LENGTH);
+}
+void main(){
+    TEST_EVP_SHA512();
+}
